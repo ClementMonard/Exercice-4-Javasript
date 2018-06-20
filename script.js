20260df7553f62420e24b6a1681b0f9b0d3ceaa1
ClementMonard <clement.monard@hotmail.fr>
2018-06-20 11:48:59 +0200

    Récupération des données du formulaire dans une boite de dialogue

---
+++ script.js
@@ -0,0 +1,7 @@
+function check() {
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+
+  alert('Nom : '+ lastname + '\nPrénom : ' + firstname + '\ncity : '+ city);
+}
