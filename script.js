4a925c2fb5e00bb664034ce2614c7621f6c40d75
ClementMonard <clement.monard@hotmail.fr>
2018-06-20 18:03:33 +0200

    Corrections majeurs et mise en place du regex

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function check() {
+//déclaration de la fonction
+function identification() {
+//Déclaration et récupération des variables
   var lastname = document.getElementById('lastname').value;
   var firstname = document.getElementById('firstname').value;
   var city = document.getElementById('city').value;
-
-  alert('Nom : '+ lastname + '\nPrénom : ' + firstname + '\ncity : '+ city);
+//Regex
+  if(/^[A-Za-z\-àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœç]+$/.test(lastname && firstname && city) == true) {
+    alert('Nom : '+ lastname + '\nPrénom : ' + firstname + '\nCity : '+ city);
+  }else {
+    alert('Ceci n\'est pas une lettre.');
+  }
 }
